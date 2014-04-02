aa31f4808ea91a944e0ab7968eccd7ff45cda45e
maxilapo <maxilapo@hotmail.com>
2014-04-02 13:51:05 -0400

    Ajout d'icône et d'une vu
    Ajout des icônes pour les procédures.
    
    Ajout d’une vu détail pour les procédures.

---
+++ ETSMobile/ETSSecurityDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ETSSecurityDetailViewController.h
+//  ETSMobile
+//
+//  Created by Maxime Lapointe on 2014-04-02.
+//  Copyright (c) 2014 ApplETS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ETSSecurityDetailViewController : UIViewController
+
+@end
