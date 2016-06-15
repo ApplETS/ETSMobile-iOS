be165724aa863428c3542ee744c6f17373cb0e43
Alyssa <alyssa.bouchenak@gmail.com>
2016-06-15 17:27:15 -0400

    Changes location of the push notifications set up Sending notifications to a user only makes sense if the user is logged in. The app now asks users if they want to receive notifications once they have logged in and clicked on the Courses view.

--- ETSMobile/ETSAppDelegate.m
+++ ETSMobile/ETSAppDelegate.m
@@ -99,39 +99,6 @@ static NSString *const SNSPlatformApplicationArn = @"arn:aws:sns:us-east-1:83488
     
     [SupportKit initWithSettings:settings];
     
-    // Sets up Mobile Push Notification
-    UIMutableUserNotificationAction *readAction = [UIMutableUserNotificationAction new];
-    readAction.identifier = @"READ_IDENTIFIER";
-    readAction.title = @"Read";
-    readAction.activationMode = UIUserNotificationActivationModeForeground;
-    readAction.destructive = NO;
-    readAction.authenticationRequired = YES;
-    
-    UIMutableUserNotificationAction *deleteAction = [UIMutableUserNotificationAction new];
-    deleteAction.identifier = @"DELETE_IDENTIFIER";
-    deleteAction.title = @"Delete";
-    deleteAction.activationMode = UIUserNotificationActivationModeForeground;
-    deleteAction.destructive = YES;
-    deleteAction.authenticationRequired = YES;
-    
-    UIMutableUserNotificationAction *ignoreAction = [UIMutableUserNotificationAction new];
-    ignoreAction.identifier = @"IGNORE_IDENTIFIER";
-    ignoreAction.title = @"Ignore";
-    ignoreAction.activationMode = UIUserNotificationActivationModeForeground;
-    ignoreAction.destructive = NO;
-    ignoreAction.authenticationRequired = NO;
-    
-    UIMutableUserNotificationCategory *messageCategory = [UIMutableUserNotificationCategory new];
-    messageCategory.identifier = @"MESSAGE_CATEGORY";
-    [messageCategory setActions:@[readAction, deleteAction] forContext:UIUserNotificationActionContextMinimal];
-    [messageCategory setActions:@[readAction, deleteAction, ignoreAction] forContext:UIUserNotificationActionContextDefault];
-    
-    UIUserNotificationType types = UIUserNotificationTypeBadge | UIUserNotificationTypeSound | UIUserNotificationTypeAlert;
-    UIUserNotificationSettings *notificationSettings = [UIUserNotificationSettings settingsForTypes:types categories:[NSSet setWithArray:@[messageCategory]]];
-    
-    [[UIApplication sharedApplication] registerUserNotificationSettings:notificationSettings];
-    [[UIApplication sharedApplication] registerForRemoteNotifications];
-    
     return YES;
 }
 
@@ -329,6 +296,42 @@ static NSString *const SNSPlatformApplicationArn = @"arn:aws:sns:us-east-1:83488
 - (NSURL *)applicationDocumentsDirectory
 {
     return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+}
+
++ (void)setUpPushNotifications
+{
+    // Sets up Mobile Push Notification
+    UIMutableUserNotificationAction *readAction = [UIMutableUserNotificationAction new];
+    readAction.identifier = @"READ_IDENTIFIER";
+    readAction.title = @"Read";
+    readAction.activationMode = UIUserNotificationActivationModeForeground;
+    readAction.destructive = NO;
+    readAction.authenticationRequired = YES;
+    
+    UIMutableUserNotificationAction *deleteAction = [UIMutableUserNotificationAction new];
+    deleteAction.identifier = @"DELETE_IDENTIFIER";
+    deleteAction.title = @"Delete";
+    deleteAction.activationMode = UIUserNotificationActivationModeForeground;
+    deleteAction.destructive = YES;
+    deleteAction.authenticationRequired = YES;
+    
+    UIMutableUserNotificationAction *ignoreAction = [UIMutableUserNotificationAction new];
+    ignoreAction.identifier = @"IGNORE_IDENTIFIER";
+    ignoreAction.title = @"Ignore";
+    ignoreAction.activationMode = UIUserNotificationActivationModeForeground;
+    ignoreAction.destructive = NO;
+    ignoreAction.authenticationRequired = NO;
+    
+    UIMutableUserNotificationCategory *messageCategory = [UIMutableUserNotificationCategory new];
+    messageCategory.identifier = @"MESSAGE_CATEGORY";
+    [messageCategory setActions:@[readAction, deleteAction] forContext:UIUserNotificationActionContextMinimal];
+    [messageCategory setActions:@[readAction, deleteAction, ignoreAction] forContext:UIUserNotificationActionContextDefault];
+    
+    UIUserNotificationType types = UIUserNotificationTypeBadge | UIUserNotificationTypeSound | UIUserNotificationTypeAlert;
+    UIUserNotificationSettings *notificationSettings = [UIUserNotificationSettings settingsForTypes:types categories:[NSSet setWithArray:@[messageCategory]]];
+    
+    [[UIApplication sharedApplication] registerUserNotificationSettings:notificationSettings];
+    [[UIApplication sharedApplication] registerForRemoteNotifications];
 }
 
 @end
